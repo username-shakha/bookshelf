ff1083017a5c64a46cb25b113523d4aa0986ad7c
username-shakha <username.shakha@gmail.com>
2024-04-01 18:29:45 +0500

    created 404 page

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,3 +1,31 @@
+import { Box, Button, styled } from "@mui/material";
+import notFoundImg from "@/assets/404.jpeg";
+import { useNavigate } from "react-router-dom";
+
+const Backdrop = styled(Box)({
+  width: "100%",
+  minHeight: "100vh",
+  display: "grid",
+  placeItems: "center",
+});
+const Actions = styled(Box)`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
 export default function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  const navigate = useNavigate();
+  return (
+    <Backdrop>
+      <img src={notFoundImg} alt="not found page" />
+      <Actions>
+        <Button onClick={() => navigate("/root")} variant="contained">
+          Go Home Page
+        </Button>
+        <Button onClick={() => navigate("/root")} variant="outlined">
+          Reload Page
+        </Button>
+      </Actions>
+    </Backdrop>
+  );
 }
