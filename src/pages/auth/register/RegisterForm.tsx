b58b4cb7a54f44f474923e35d0cddc8f07b54d13
username-shakha <username.shakha@gmail.com>
2024-04-01 22:33:10 +0500

    fix register token

--- src/pages/auth/register/RegisterForm.tsx
+++ src/pages/auth/register/RegisterForm.tsx
@@ -1,9 +1,9 @@
 import { useForm } from "react-hook-form";
 import { Button, Stack } from "@mui/material";
 import { FormInput } from "@/components/Form/FormInput";
-// import { setToken, removeToken } from "@/utils/token";
+import { setToken, removeToken } from "@/utils/token";
 
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 type TInputFieldTypes = {
   username: string;
@@ -11,34 +11,34 @@ type TInputFieldTypes = {
 };
 
 export default function RegisterForm() {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  const { control } = useForm<TInputFieldTypes>();
+  const { control, handleSubmit } = useForm<TInputFieldTypes>();
 
-  // const submitRegister = async (registerData: TInputFieldTypes) => {
-  //   try {
-  //     const response = await fetch(`${import.meta.env.VITE_API_URL}/register`, {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify(registerData),
-  //     });
-  //     const result = await response.json();
+  const submitRegister = async (registerData: TInputFieldTypes) => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(registerData),
+      });
+      const result = await response.json();
 
-  //     if (result.token !== undefined) {
-  //       setToken(result.token);
-  //       navigate("/root");
-  //     }
-  //   } catch (error: unknown) {
-  //     removeToken();
-  //     console.error((error as Error).message);
-  //     throw new Error((error as Error).message);
-  //   }
-  // };
-  //onSubmit={handleSubmit((data) => submitRegister(data))}
+      if (result.token !== undefined) {
+        setToken(result.token);
+        navigate("/");
+      }
+    } catch (error: unknown) {
+      removeToken();
+      console.error((error as Error).message);
+      throw new Error((error as Error).message);
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit((data) => submitRegister(data))}>
       <Stack gap={2} pb={1.5}>
         <FormInput
           {...{ control, name: "username" }}
