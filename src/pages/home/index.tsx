f5623d4e0d14763d393419b7e87d1f82c96b6f71
username-shakha <username.shakha@gmail.com>
2024-05-05 14:43:24 +0500

    change test api button group

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,14 @@ import { useNavigate } from "react-router-dom";
 import BookList from "@/components/BookList";
 import { getUserToken } from "@/utils/token";
 
+// api test
+// import useUserManagement from "@/hooks/useUserManagement";
+// import { Button, Stack } from "@mui/material";
+
 export default function HomePage() {
+  // api test
+  // const { getUserInfo, removeBook, editBook, searchBook, addBook } = useUserManagement();
+
   const navigate = useNavigate();
   useEffect(() => {
     const userToken = getUserToken();
@@ -12,23 +19,27 @@ export default function HomePage() {
     }
   }, [navigate]);
   return (
-    <div>
-      {/* <Button onClick={() => addBook("978-5-04-102313-3")} variant="contained">
-        Test addBook
-      </Button>
-      <Button variant="contained" onClick={() => getUserInfo()}>
-        Test Get User info
-      </Button>
-      <Button variant="contained" onClick={() => removeBook(131)}>
-        Test Remove Book
-      </Button>
-      <Button variant="contained" onClick={() => editBook({ id: 130, statuses: 1 })}>
-        Test Edit Book
-      </Button>
-      <Button variant="contained" onClick={() => searchBook("Raspberry Pi User Guide")}>
-        Test Search Book
-      </Button> */}
+    <>
+      {/* api test */}
+      {/* <Stack spacing={2} direction="row" mx={"auto"} my={2} width="fit-content">
+        <Button onClick={() => addBook("978-5-04-102313-3")} variant="contained">
+          Test addBook
+        </Button>
+        <Button variant="contained" onClick={() => getUserInfo()}>
+          Test Get User info
+        </Button>
+        <Button variant="contained" onClick={() => removeBook(131)}>
+          Test Remove Book
+        </Button>
+        <Button variant="contained" onClick={() => editBook({ id: 130, statuses: 1 })}>
+          Test Edit Book
+        </Button>
+        <Button variant="contained" onClick={() => searchBook("Raspberry Pi User Guide")}>
+          Test Search Book
+        </Button>
+      </Stack> */}
+
       <BookList />
-    </div>
+    </>
   );
 }
