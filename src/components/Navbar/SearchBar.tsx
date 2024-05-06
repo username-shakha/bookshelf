c2e1eb865dd52a918d11ce78b694b31695d69c84
username-shakha <username.shakha@gmail.com>
2024-05-06 11:46:40 +0500

    change search bar

--- src/components/Navbar/SearchBar.tsx
+++ src/components/Navbar/SearchBar.tsx
@@ -9,9 +9,9 @@ import { isShowSeachBarInput } from "@/utils";
 export default function SearchBar() {
   const { searchBook } = useUserManagement();
   const [showSearch, setShowSearch] = useState(false);
-
   const [inputValue, setInputValue] = useState("");
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    searchBook(e.target.value).then((res) => console.log(res));
     setInputValue(e.target.value);
   };
 
@@ -22,32 +22,34 @@ export default function SearchBar() {
   }, [showSearch]);
 
   return (
-    <TextField
-      disabled={!showSearch}
-      sx={isShowSeachBarInput(showSearch)}
-      type="text"
-      value={inputValue}
-      onChange={handleChange}
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <Tooltip title="Open Search">
-              <IconButton onClick={() => setShowSearch(true)} disabled={showSearch}>
-                <SearchIcon style={{ stroke: `${showSearch ? "#000" : "#fff"}` }} />
-              </IconButton>
-            </Tooltip>
-          </InputAdornment>
-        ),
-        endAdornment: showSearch && (
-          <InputAdornment position="end">
-            <Tooltip title="Close Search">
-              <IconButton onClick={() => setShowSearch(false)}>
-                <CloseIcon style={{ stroke: "#333333" }} />
-              </IconButton>
-            </Tooltip>
-          </InputAdornment>
-        ),
-      }}
-    />
+    <>
+      <TextField
+        disabled={!showSearch}
+        sx={isShowSeachBarInput(showSearch)}
+        type="text"
+        value={inputValue}
+        onChange={handleChange}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <Tooltip title="Open Search">
+                <IconButton onClick={() => setShowSearch(true)} disabled={showSearch}>
+                  <SearchIcon style={{ stroke: `${showSearch ? "#000" : "#fff"}` }} />
+                </IconButton>
+              </Tooltip>
+            </InputAdornment>
+          ),
+          endAdornment: showSearch && (
+            <InputAdornment position="end">
+              <Tooltip title="Close Search">
+                <IconButton onClick={() => setShowSearch(false)}>
+                  <CloseIcon style={{ stroke: "#333333" }} />
+                </IconButton>
+              </Tooltip>
+            </InputAdornment>
+          ),
+        }}
+      />
+    </>
   );
 }
